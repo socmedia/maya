009880811b706c34ce797c604ebb1e679f2ee733
indraranuh-dev <61109346+indraranuh-dev@users.noreply.github.com>
2020-09-27 22:58:23 +0700

    Styling product cards

--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -1,96 +1,138 @@
 <section id="product">
     <div class="container">
         <div class="row">
+            <div class="col-12 mb-5">
+                <h2 class="header-text">Semua <span>Produk</span></h2>
+            </div>
             <div class="col-12 col-md-4">
                 <div class="product__card">
-                    <div class="product__image">
-                        <img src="{{asset('img/product/crystal.png')}}" alt="Crystal">
-                    </div>
-                    <div class="product__info">
-                        <h2 class="product__name">Crystal</h2>
-                        <p class="product__description">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptate mollitia earum
-                            debitis neque nemo doloremque odit error totam minima eius amet excepturi voluptas,
-                            accusamus beatae voluptates iure expedita delectus.
-                        </p>
-                    </div>
+                    <a href="javascript:void(0)" data-slug="elegance" show-modal>
+                        <div class="product__image">
+                            <img src="{{asset('img/product/elegance.png')}}" alt="Elegance">
+                        </div>
+                        <div class="product__info">
+                            <h2 class="product__name"><span>Elegance</span></h2>
+                            <p class="product__description">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptate mollitia
+                                earum
+                                debitis neque nemo doloremque odit error totam minima eius amet excepturi voluptas,
+                                accusamus beatae voluptates iure expedita delectus.
+                            </p>
+                        </div>
+                    </a>
                 </div>
             </div>
             <div class="col-12 col-md-4">
                 <div class="product__card">
-                    <div class="product__image">
-                        <img src="{{asset('img/product/elegance.png')}}" alt="Elegance">
-                    </div>
-                    <div class="product__info">
-                        <h2 class="product__name">Elegance</h2>
-                        <p class="product__description">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptate mollitia earum
-                            debitis neque nemo doloremque odit error totam minima eius amet excepturi voluptas,
-                            accusamus beatae voluptates iure expedita delectus.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-md-4">
-                <div class="product__card">
-                    <div class="product__image">
-                        <img src="{{asset('img/product/flipper.png')}}" alt="Flipper">
-                    </div>
-                    <div class="product__info">
-                        <h2 class="product__name">Flipper</h2>
-                        <p class="product__description">
+                    <a href="javascript:void(0)" data-slug="flipper" show-modal>
+                        <div class="product__image">
+                            <img src="{{asset('img/product/flipper.png')}}" alt="Flipper">
+                        </div>
+                        <div class="product__info">
+                            <h2 class="product__name"><span>Flipper</span></h2>
+                            <p class="product__description">
 
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptate mollitia earum
-                            debitis neque nemo doloremque odit error totam minima eius amet excepturi voluptas,
-                            accusamus beatae voluptates iure expedita delectus. </p>
-                    </div>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptate mollitia
+                                earum
+                                debitis neque nemo doloremque odit error totam minima eius amet excepturi voluptas,
+                                accusamus beatae voluptates iure expedita delectus. </p>
+                        </div>
+                </div>
+                </a>
+            </div>
+            <div class="col-12 col-md-4">
+                <div class="product__card">
+                    <a href="javascript:void(0)" data-slug="elegance-2" show-modal>
+                        <div class="product__image">
+                            <img src="{{asset('img/product/elegance 2.png')}}" alt="Elegance 2">
+                        </div>
+                        <div class="product__info">
+                            <h2 class="product__name"><span>Elegance 2</span></h2>
+                            <p class="product__description">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptate mollitia
+                                earum
+                                debitis neque nemo doloremque odit error totam minima eius amet excepturi voluptas,
+                                accusamus beatae voluptates iure expedita delectus.
+                            </p>
+                        </div>
+                    </a>
                 </div>
             </div>
             <div class="col-12 col-md-4">
                 <div class="product__card">
-                    <div class="product__image">
-                        <img src="{{asset('img/product/elegance 2.png')}}" alt="Foldable">
-                    </div>
-                    <div class="product__info">
-                        <h2 class="product__name">Foldable</h2>
-                        <p class="product__description">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptate mollitia earum
-                            debitis neque nemo doloremque odit error totam minima eius amet excepturi voluptas,
-                            accusamus beatae voluptates iure expedita delectus.
-                        </p>
-                    </div>
+                    <a href="javascript:void(0)" data-slug="prestige" show-modal>
+                        <div class="product__image">
+                            <img src="{{asset('img/product/prestige.png')}}" alt="Prestige">
+                        </div>
+                        <div class="product__info">
+                            <h2 class="product__name"><span>Prestige</span></h2>
+                            <p class="product__description">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptate mollitia
+                                earum
+                                debitis neque nemo doloremque odit error totam minima eius amet excepturi voluptas,
+                                accusamus beatae voluptates iure expedita delectus.
+                            </p>
+                        </div>
+                    </a>
                 </div>
             </div>
             <div class="col-12 col-md-4">
                 <div class="product__card">
-                    <div class="product__image">
-                        <img src="{{asset('img/product/prestige.png')}}" alt="Prestige">
-                    </div>
-                    <div class="product__info">
-                        <h2 class="product__name">Prestige</h2>
-                        <p class="product__description">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptate mollitia earum
-                            debitis neque nemo doloremque odit error totam minima eius amet excepturi voluptas,
-                            accusamus beatae voluptates iure expedita delectus.
-                        </p>
-                    </div>
+                    <a href="javascript:void(0)" data-slug="sporty" show-modal>
+                        <div class="product__image">
+                            <img src="{{asset('img/product/sporty.png')}}" alt="Sporty">
+                        </div>
+                        <div class="product__info">
+                            <h2 class="product__name"><span>Sporty</span></h2>
+                            <p class="product__description">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptate mollitia
+                                earum
+                                debitis neque nemo doloremque odit error totam minima eius amet excepturi voluptas,
+                                accusamus beatae voluptates iure expedita delectus.
+                            </p>
+                        </div>
+                    </a>
                 </div>
             </div>
             <div class="col-12 col-md-4">
                 <div class="product__card">
-                    <div class="product__image">
-                        <img src="{{asset('img/product/sporty.png')}}" alt="Sporty">
-                    </div>
-                    <div class="product__info">
-                        <h2 class="product__name">Sporty</h2>
-                        <p class="product__description">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptate mollitia earum
-                            debitis neque nemo doloremque odit error totam minima eius amet excepturi voluptas,
-                            accusamus beatae voluptates iure expedita delectus.
-                        </p>
-                    </div>
+                    <a href="javascript:void(0)" data-slug="crystal" show-modal>
+                        <div class="product__image">
+                            <img src="{{asset('img/product/crystal.png')}}" alt="Crystal">
+                        </div>
+                        <div class="product__info">
+                            <h2 class="product__name"><span>Crystal</span></h2>
+                            <p class="product__description">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptate mollitia
+                                earum
+                                debitis neque nemo doloremque odit error totam minima eius amet excepturi voluptas,
+                                accusamus beatae voluptates iure expedita delectus.
+                            </p>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
     </div>
 </section>
+
+<div class="modal fade" id="modal-description" tabindex="-1" role="dialog" aria-labelledby="modelTitleId"
+    aria-hidden="true" data-aos="fade-down">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <h5 class="modal-title">Modal title</h5>
+                <div class="product-image"></div>
+                <div class="product-info"></div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>

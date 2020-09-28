be1b19438ce1dd1bde06522e1df576fe86e731a7
socmedia <adm1.socmedia@gmail.com>
2020-09-28 15:03:37 +0700

    Add coming soon page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maya Spring Bed</title>
+    <link rel="stylesheet" href="{{asset('dist/css/app.css')}}">
+    <link rel="shortcut icon" href="{{asset('dist/img/icofont.png')}}" type="image/x-icon">
+</head>
+
+<body>
+    <div class="logo">
+        <img src="dist/img/logo.png" alt="logo">
+    </div>
+    <div class="wrapper">
+        <img class="desktop" src="{{asset('dist/img/all.png')}}" alt="image">
+        <img class="mobile" src="{{asset('dist/img/all-sm.png')}}" alt="image">
+    </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('landing.index');
+    return view('index');
 });
 
 Route::get('/demo', function () {

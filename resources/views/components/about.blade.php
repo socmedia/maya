1beddada12741ba2e878a8ae42d7c493f8f05d98
indraranuh-dev <61109346+indraranuh-dev@users.noreply.github.com>
2020-09-29 21:41:50 +0700

    Styling features section

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -1,57 +1,71 @@
 <div id="about">
     <div class="container">
-        <div class="row mb-3">
-            <div class="col-12 col-md-6 mb-3 mb-md-0">
+        <div class="row mb-3 justify-content-center">
+            <div class="col-12 col-md-10 mb-3 mb-md-0">
                 <div class="about-text">
                     <h2>Kesempurnaan <span>Jahitan</span></h2>
                     <hr>
-                    <p>
-                        Kasur Maya Springbed menggunakan mesin jahit yang mempunyai 288 jarum. Semua perpaduan bentuk
-                        jahitan lengkung, bulat, atau garis bisa dihasilkan dengan sempurna tanpa putus sehingga kasur
-                        terlihat lebih elegan.
-                    </p>
                 </div>
-            </div>
-            <div class="col-12 col-md-6">
-                <div class="about-media"> <img src="{{asset('img/jahitan.png')}}" alt="7 Support Zones"></div>
+                <div class="about-body">
+                    <div class="about-body__img ml-0">
+                        <img class="mr-0" src="{{asset('img/jahitan.png')}}" alt="7 Support Zones">
+                    </div>
+                    <div class="about-body__text">
+                        <p>
+                            Kasur Maya Springbed menggunakan mesin jahit yang mempunyai 288 jarum. Semua perpaduan
+                            bentuk
+                            jahitan lengkung, bulat, atau garis bisa dihasilkan dengan sempurna tanpa putus sehingga
+                            kasur
+                            terlihat lebih elegan.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
 
-        <div class="row mb-3">
-            <div class="col-12 col-md-6 mb-3 mb-md-0">
-                <div class="about-media"> <img src="{{asset('img/pegas.png')}}" alt="7 Support Zones"></div>
-            </div>
-            <div class="col-12 col-md-6">
+        <div class="row mb-3 justify-content-center">
+            <div class="col-12 col-md-10 mb-3 mb-md-0">
                 <div class="about-text">
                     <h2>Pegas <span>Terbaik</span></h2>
                     <hr>
-                    <p>
-                        Kasur Maya Springbed mempunyai semua kelebihan yang diperlukan untuk kesehatan tubuh. Pillow top
-                        setinggi 12 cm untuk kenyamanan tidur dan tampilan yang mewah, 7 support zones untuk kesehatan
-                        tulang belakang, tambahan lapisan rebonded foam yang mempunyai tingkat kepadatan di atas 65
-                        sehingga kemembalannya sempurna.
-                    </p>
+                </div>
+                <div class="about-body col-reverse-md">
+                    <div class="about-body__text">
+                        <p>
+                            Kasur Maya Springbed mempunyai semua kelebihan yang diperlukan untuk kesehatan tubuh. Pillow
+                            top
+                            setinggi 12 cm untuk kenyamanan tidur dan tampilan yang mewah, 7 support zones untuk
+                            kesehatan
+                            tulang belakang, tambahan lapisan rebonded foam yang mempunyai tingkat kepadatan di atas 65
+                            sehingga kemembalannya sempurna.
+                        </p>
+                    </div>
+                    <div class="about-body__img mr-0">
+                        <img src="{{asset('img/pegas.png')}}" alt="7 Support Zones">
+                    </div>
                 </div>
             </div>
         </div>
 
-        <div class="row">
-            <div class="col-12 col-md-6 mb-3 mb-md-0">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-10 mb-3 mb-md-0">
                 <div class="about-text">
                     <h2>7 Support <span>Zones</span></h2>
                     <hr>
-                    <p>Kasur lateks European Bedding dirancang dengan 7 zona kenyamanan untuk dukungan dan kenyamanan
-                        tubuh yang maksimal. Setiap zona sesuai secara individual dengan bagian tubuh Anda yang berbeda.
-                        Zona bahu sedikit lebih lembut, karena bahu kita lebih menonjol. Zona tengah sedikit lebih
-                        kencang untuk menopang area pinggang kita, tempat bersandar pada bagian terberat tubuh kita.
-                        Sistem pendukung yang dikategorikan ini menjaga tulang belakang kita dalam posisi aslinya pada
-                        malam hari. Selain itu, dengan menopang bobot kami oleh zona tengah yang lebih kokoh akan
-                        mencegah kasur melorot dan menjamin masa pakai kasur Anda lebih lama.
-                    </p>
                 </div>
-            </div>
-            <div class="col-12 col-md-6">
-                <div class="about-media"> <img src="{{asset('img/7support.png')}}" alt="7 Support Zones"></div>
+                <div class="about-body">
+                    <div class="about-body__img ml-0">
+                        <img src="{{asset('img/7support.png')}}" alt="7 Support Zones">
+                    </div>
+                    <div class="about-body__text">
+                        <p>
+                            Kasur lateks European Bedding dirancang dengan 7 zona kenyamanan untuk dukungan dan
+                            kenyamanan
+                            tubuh yang maksimal. Setiap zona sesuai secara individual dengan bagian tubuh Anda yang
+                            berbeda.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
